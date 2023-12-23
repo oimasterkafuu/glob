d1ffb154a31651281e0f2e53db1a5947015eb421
oimasterkafuu <oimasterfake@icloud.com>
2023-12-23 18:09:15 +0800

    feat: markdown support

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,76 @@
-<template>Hello, world!</template>
+<template>
+    <div id="content"></div>
+</template>
 
 <script>
 export default {
-    name: 'Home'
+    name: 'Home',
+    data() {
+        return {
+            content: `# Glob
+
+Get Lit, Oimasterkafuu's Blog. 点亮吧，Oimasterkafuu 的博客！
+
+这是一个无服务器博客，正在开发中。它使用 Vue 作为前端，Leancloud 作为后端，以及 Vite 作为构建工具。
+
+可能后面会有更详细的介绍……现在内容那么少，我也不清楚会发生什么。
+
+不愧是 <span style="color: #8db3f1;">oimasterkafuu<span class="ui tiny label" style="display: inline-block; color: white; background-color: #8db3f1">かわいい</span></span>！
+
+# Markdown测试文章
+
+## 标题
+
+### 小标题
+
+#### 更小的标题
+
+##### 还要小的标题
+
+###### 最小的标题
+
+## 文本格式
+
+* **粗体**：**粗体**
+* *斜体*：*斜体*
+* ~~删除线~~：~~删除线~~
+
+## 列表
+
+* 无序列表：
+    * 第一项
+    * 第二项
+    * 第三项
+* 有序列表：
+    1. 第一项
+    2. 第二项
+    3. 第三项
+
+## 代码块
+
+\`\`\`python
+print("Hello, world!")
+\`\`\`
+
+## 链接
+
+[Google](https://www.google.com)
+
+## 图片
+
+![oimaster](https://gravatar.oimaster.top/avatar/c2f21b7c17cbc4aecabfa3e44b58d164?size=800)
+
+## 表格
+
+| 表头 | 表头 | 表头 |
+|---|---|---|
+| 内容 | 内容 | 内容 |
+| 内容 | 内容 | 内容 |
+`
+        };
+    },
+    mounted() {
+        $('#content').html(marked.parse(this.content));
+    }
 };
 </script>
