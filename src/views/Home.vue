3bc892a684c7be2c3d4cf95d5e0a902f1b966be8
oimasterkafuu <oimasterfake@icloud.com>
2023-12-23 20:21:12 +0800

    fix: add markdown features to semantic-ui
    fix #1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,11 +11,13 @@ export default {
 
 Get Lit, Oimasterkafuu's Blog. 点亮吧，Oimasterkafuu 的博客！
 
-这是一个无服务器博客，正在开发中。它使用 Vue 作为前端，Leancloud 作为后端，以及 Vite 作为构建工具。
+> 这是一个无服务器博客，正在开发中。它使用 Vue 作为前端，Leancloud 作为后端，以及 Vite 作为构建工具。
 
 可能后面会有更详细的介绍……现在内容那么少，我也不清楚会发生什么。
 
 不愧是 <span style="color: #8db3f1;">oimasterkafuu<span class="ui tiny label" style="display: inline-block; color: white; background-color: #8db3f1">かわいい</span></span>！
+
+---
 
 # Markdown测试文章
 
@@ -63,9 +65,10 @@ print("Hello, world!")
 ## 表格
 
 | 表头 | 表头 | 表头 |
-|---|---|---|
+|:--|--:|:--:|
 | 内容 | 内容 | 内容 |
 | 内容 | 内容 | 内容 |
+
 
 ## 公式
 
@@ -75,8 +78,47 @@ $ 1 + 2 = 3 $ 是一道简单的算术题。
         };
     },
     mounted() {
+        const renderer = {
+            table: (header, body) => {
+                return (
+                    '<table class="ui unstackable celled table"><thead>' +
+                    header +
+                    '</thead><tbody>' +
+                    body +
+                    '</tbody></table>'
+                );
+            },
+            tablecell: (content, flags) => {
+                var tag = 'td';
+                if (flags.header) {
+                    tag = 'th';
+                }
+                return (
+                    '<' +
+                    tag +
+                    (flags.align && flags.align !== 'left' ? ' class="' + flags.align + ' aligned"' : '') +
+                    '>' +
+                    content +
+                    '</' +
+                    tag +
+                    '>'
+                );
+            },
+            image: (href, title, text) => {
+                return '<div class="ui image"><img src="' + href + '" alt="' + text + '"></div>';
+            },
+            blockquote: (text) => {
+                return '<blockquote class="ui segment">' + text + '</blockquote>';
+            },
+            hr: () => {
+                return '<div class="ui divider"></div>';
+            }
+        };
+
+        marked.use({ renderer });
+
         $('#content').html(marked.parse(this.content));
-        
+
         renderMathInElement($('#content')[0], {
             delimiters: [
                 { left: '$$', right: '$$', display: true },
