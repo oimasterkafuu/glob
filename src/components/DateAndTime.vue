d3fc91702ef22d21d83c520b97e848b512df75a2
oimasterkafuu <oimasterfake@icloud.com>
2023-12-24 10:29:48 +0800

    feat: add date and time

---
+++ src/components/DateAndTime.vue
@@ -0,0 +1,22 @@
+<template>
+    {{ formattedTime }}
+</template>
+
+<script>
+import moment from 'moment';
+import 'moment/dist/locale/zh-cn';
+
+export default {
+    name: 'DateAndTime',
+    props: {
+        time: {
+            type: Date
+        }
+    },
+    computed: {
+        formattedTime() {
+            return moment(this.time).fromNow();
+        }
+    }
+};
+</script>
