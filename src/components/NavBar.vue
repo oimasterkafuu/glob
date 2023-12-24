0e575f1621130b0a3309afd33ee81d363f9ca2d9
oimasterkafuu <oimasterfake@icloud.com>
2023-12-24 12:11:48 +0800

    feat: add admin in navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="ui top fixed borderless menu">
         <div class="ui container">
-            <RouterLink to="/" class="header item"> <div class="logo">oimaster</div> </RouterLink>
+            <RouterLink to="/" class="header item">
+                <div class="logo">oimaster</div>
+            </RouterLink>
 
             <RouterLink to="/" class="item" :class="{ active: $route.path === '/' }">
                 <i class="home icon"></i>
@@ -13,15 +15,20 @@
                 文章
             </RouterLink>
 
-            <a
-                href="https://github.com/oimasterkafuu/glob"
-                class="item mobile-hide"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <i class="heart icon"></i>
-                开源
-            </a>
+            <div class="right menu mobile-hide">
+                <a href="https://github.com/oimasterkafuu/glob" class="item" target="_blank" rel="noopener noreferrer">
+                    <i class="heart icon"></i>
+                    开源
+                </a>
+                <RouterLink to="/admin" class="item" :class="{ active: $route.path === '/admin' }" v-if="user">
+                    <i class="cog icon"></i>
+                    后台
+                </RouterLink>
+                <a class="item" v-if="user" @click="logOut()">
+                    <i class="sign out icon"></i>
+                    登出
+                </a>
+            </div>
         </div>
     </div>
 </template>
@@ -57,6 +64,17 @@ div.logo {
 
 <script>
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    data() {
+        return {
+            user: AV.User.current()
+        };
+    },
+    methods: {
+        logOut() {
+            AV.User.logOut();
+            location.reload();
+        }
+    }
 };
 </script>
