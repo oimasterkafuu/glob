8f5c3b7ae7e3637b4f43613d50fcada1f5d56cb1
oimasterkafuu <oimasterfake@icloud.com>
2023-12-24 17:37:13 +0800

    fix: home page limit type not correct

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <MarkerUrl url="/Home.md" :safe="true"></MarkerUrl>
     <h2>最新文章</h2>
-    <ArticleCards limit="2"></ArticleCards>
+    <ArticleCards :limit="2"></ArticleCards>
 </template>
 
 <script>
